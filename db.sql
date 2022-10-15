dd120652ebaf103de26ddcf2a3fbb892c8e89426
JackLove <gogkatsu@outlook.com>
2022-10-15 11:52:50 +0800

    update sql

--- db.sql
+++ db.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `company`
 --
 
+DROP DATABASE IF EXISTS cms;
+create DATABASE cms; 
+use cms; 
+
 DROP TABLE IF EXISTS `company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
